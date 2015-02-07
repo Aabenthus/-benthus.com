4776b66c963372d7aca9451b668f1781cfc19026
Kræn Hansen (Socialsquare) <kraen@socialsquare.dk>
2015-02-08 00:08:21 +0100

    Changed the credentials field to a textfield

--- aabenthus_com/google/models.py
+++ aabenthus_com/google/models.py
@@ -1,8 +1,15 @@
 from django.db import models
 
+import json
+
 class Authorization(models.Model):
 	email = models.CharField(max_length=256)
-	credentials = models.CharField(max_length=1024)
+	credentials = models.TextField()
 	
 	def __str__(self):
 		return 'Authorization %s' % ( self.email )
+
+	def refresh_if_needed(self):
+		credentials = json.loads(self.credentials)
+		print(credentials.get('refresh_token'))
+		pass
