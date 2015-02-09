ed00ca495ffc52bae9cd3b334f259366b48e3d9e
Kræn Hansen (Socialsquare) <kraen@socialsquare.dk>
2015-02-09 21:51:48 +0100

    Changing the secret key to be loaded from the environment, and debug False.

--- aabenthus_com/settings.py
+++ aabenthus_com/settings.py
@@ -18,12 +18,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'dont-even-bother'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-TEMPLATE_DEBUG = True
+TEMPLATE_DEBUG = False
 
 ALLOWED_HOSTS = []
 
