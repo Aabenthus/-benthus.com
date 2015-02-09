f3ee8ba442bcce22094ac26f4a90709352d99473
Kræn Hansen (Socialsquare) <kraen@socialsquare.dk>
2015-02-09 21:52:30 +0100

    Reverting - we'll use a custom settings file instead.

--- aabenthus_com/settings.py
+++ aabenthus_com/settings.py
@@ -18,12 +18,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = 'dont-even-bother'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-TEMPLATE_DEBUG = False
+TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = []
 
